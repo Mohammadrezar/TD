77e5f9951c69380f405017cb944c8aa62ce7fe45
Mohammad <mrr1377525@gmail.com>
2016-10-10 21:59:30 +0330

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -208,7 +208,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-		text = "Welcome to TeleSeed!\n\nTo get a list of TeleSeed groups use /chats or /chatlist for a document list of chats.\n\nTo get a new TeleSeed group, contact a support group:\n\nFor English support, use: /join English support\n\nFor Persian support, use: /join Persian support\n\nFor more information, check out our channels:\n\n@TeleseedCH [English]\n@Iranseed [Persian]\n\nThanks for using @TeleSeed!"
+		text = "Welcome to TeleDiamond!\n\nTo get a list of TeleDiamond groups use /chats or /chatlist for a document list of chats.\n\nTo get a new TeleDiamond group, contact a support group:\n\nFor English support, use: /join us\n@antispamandhack"
      	return text
     end
 
