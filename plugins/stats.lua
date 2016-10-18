1e7ee62de732c942c6868260493f8f3240636853
Mohammad <mrr1377525@gmail.com>
2016-10-19 02:54:01 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -94,7 +94,7 @@ local function run(msg, matches)
   if matches[1]:lower() == 'telediamond' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /telediamond ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
